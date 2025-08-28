3657bfb4e4ebda17e9b76368e3d79d3ce2a1d2a3
Joao vitor <joao_oliveira171@estudante.sesisenai.org.br>
2025-08-28 19:47:41 -0300

    asdad

--- baralho.py
+++ baralho.py
@@ -1,9 +1,7 @@
 import random
 
 def criar_baralho():
-    tipos = ['♠', '♥', '♦', '♣']
-    valores = [2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 11]
-    baralho = [(valor, tipo) for valor in valores for tipo in tipos]
+    baralho = [2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 11] * 4
     random.shuffle(baralho)
     return baralho
 

4d0cbc4a64e0d167218394449e34e727d105e9bf
Tiago Vargas Gonçalves <tiago_v_goncalves@estudante.sesisenai.org.br>
2025-08-29 21:07:06 -0300

    fidsuivbs

--- baralho.py
+++ baralho.py
@@ -1,14 +1,20 @@
 import random
 
+import random
+
 def criar_baralho():
+    
     baralho = [2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 11] * 4
     random.shuffle(baralho)
     return baralho
 
 def distribuir_carta(baralho, trapacear=False, pontos_jogador=0):
     if trapacear and pontos_jogador >= 18:
-        for carta in sorted(baralho, reverse=True):
-            if pontos_jogador < carta <= 21:
-                baralho.remove(carta) 
-                return carta
+    
+        for i, carta in enumerate(sorted(baralho, reverse=True)):
+           
+            if pontos_jogador + carta <= 21:
+                e
+                return baralho.pop(baralho.index(carta))
+    
     return baralho.pop()
